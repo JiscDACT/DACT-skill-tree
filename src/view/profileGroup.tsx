277ba9cd56d9ccc0e7385fe0c6bc0a4650e85cee
scottwilson <scott.bradley.wilson@gmail.com>
2023-07-26 17:32:53 +0100

    feature: implemented filtering of profiles

--- src/view/profileGroup.tsx
+++ src/view/profileGroup.tsx
@@ -1,15 +1,66 @@
 import {Profile} from "./profile";
-import React from "react";
+import React, {useState} from "react";
 import {SkillProfile} from "../model/skillProfile";
 
 export function ProfileGroup(props:any){
+
+    const categories = ['Any', 'Other', 'Epic', 'Squad']
+    const squads = ['Any', 'Cheetah', 'Squadge', 'Beta Blockers']
+    const levels = ['Any', 'Beginner', 'Intermediate', 'Advanced']
+
+    const [category, setCategory] = useState('Any')
+    const [squad, setSquad] = useState('Any')
+    const [level, setLevel] = useState('Any')
+
+    const filteredProfiles = () => {
+        return props.profiles.filter((profile: { category: string; squad:string; level:string }) =>
+            (category === 'Any' || (profile.category && profile.category === category))
+            &&
+            (squad === 'Any' || (profile.squad && profile.squad === squad))
+            &&
+            (level === 'Any' || (profile.level && profile.level === level))
+        );
+    }
+
     return (
+            <div>
+                <div className={'filter-group'}>
+                    <p>Profile type <select onChange={(e) => setCategory(e.target.value)}>
+                        {
+                            categories.map((cat) => {
+                                return <option key={cat}>{cat}</option>
+                            })
+                        }
+                    </select>
+                    </p>
+
+                    <p>Squad <select onChange={(e) => setSquad(e.target.value)}>
+                            {
+                                squads.map((cat) => {
+                                    return <option key={cat}>{cat}</option>
+                                })
+                            }
+                        </select>
+                    </p>
+
+                    <p>Level <select onChange={(e) => setLevel(e.target.value)}>
+                        {
+                            levels.map((cat) => {
+                                return <option key={cat}>{cat}</option>
+                            })
+                        }
+                    </select>
+                    </p>
+                </div>
+
+
             <div className={'profile-group'}>
             {
-                props.profiles.map(function(profile:SkillProfile, key=profile.name){
+                filteredProfiles().map(function(profile:SkillProfile, key=profile.name){
                 return <Profile key={key} data={profile}/>;
                 })
             }
+            </div>
             </div>
     )
 }
