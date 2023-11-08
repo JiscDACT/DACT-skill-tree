37977c1c59f76517ccbb8225913597f1ffe08b4a
jennifer.cooper <jennifer.cooper@jisc.ac.uk>
2023-11-08 18:16:26 +0000

    feat: adding a full skil tree resource feature at the bottom of the page and associated css. Involved adding to the treeWrapper.

--- src/view/treeWrapper.tsx
+++ src/view/treeWrapper.tsx
@@ -1,26 +1,38 @@
 import React, { Component, MouseEvent } from "react";
 import { SkillGroupDataType, SkillProvider, SkillTree, SkillTreeGroup } from "beautiful-skill-tree";
+import Button from '@material-ui/core/Button';
+
 
 type TreeWrapperProps = {
     treeId: string;
-    tree: any;
+    tree: any[];
     title: string;
     handleSave: any;
     onSkillClick?: (skill: any) => void;
-}
+};
 
-export class TreeWrapper extends Component<TreeWrapperProps> {
+type TreeWrapperState = {
+    showFullInfo: boolean;
+};
+
+type LinkType = {
+    url: string;
+    text: string;
+};
+
+export class TreeWrapper extends Component<TreeWrapperProps, TreeWrapperState> {
+    state: TreeWrapperState = {
+        showFullInfo: false
+    };
 
     modifyIconPaths(tree: any[]): any[] {
         return tree.map(skill => {
             let updatedSkill = { ...skill };
 
-            // Modify icon path
             if (updatedSkill.icon) {
                 updatedSkill.icon = `${process.env.PUBLIC_URL}/${updatedSkill.icon}`;
             }
 
-            // Recursively process children
             if (updatedSkill.children && updatedSkill.children.length > 0) {
                 updatedSkill.children = this.modifyIconPaths(updatedSkill.children);
             }
@@ -41,7 +53,7 @@ export class TreeWrapper extends Component<TreeWrapperProps> {
         }
     };
 
-    findSkillById(id: string, tree = this.props.tree): any {
+    findSkillById(id: string, tree: any[] = this.props.tree): any {
         for (const skill of tree) {
             if (skill.id === id) return skill;
             if (skill.children) {
@@ -52,29 +64,81 @@ export class TreeWrapper extends Component<TreeWrapperProps> {
         return null;
     };
 
+    toggleFullInfo = () => {
+        this.setState(prevState => ({
+            showFullInfo: !prevState.showFullInfo
+        }));
+    };
+
+    renderSkill = (skill: any) => {
+        return (
+            <div key={skill.id} style={{ marginBottom: '50px' }}>
+                <h2 style={{ fontWeight: 'bold', marginBottom: '10px' }}>Skill: {skill.title}</h2>
+                <p style={{ marginBottom: '10px' }}>Overview: {skill.tooltip.content}</p>
+                {skill.links && skill.links.map((link: LinkType, index: number) => (
+                    <div key={index} style={{ marginBottom: index === skill.links.length - 1 ? '30px' : '10px' }}>
+                        <h4 style={{ margin: '10px 0' }}>Guidance: {link.text}</h4>
+                        <a href={link.url} target="_blank" rel="noopener noreferrer">{link.url}</a>
+                    </div>
+                ))}
+            </div>
+        );
+    };
+
+
+    recursiveRenderSkills = (skills: any[]): any => {
+        return skills.map(skill => (
+            <React.Fragment key={skill.id}>
+                {this.renderSkill(skill)}
+                {skill.children && this.recursiveRenderSkills(skill.children)}
+            </React.Fragment>
+        ));
+    };
+
+    renderFullInfo = () => {
+        return (
+            <div className="full-tree-info">
+                <h1 style={{ marginBottom: '60px' }}>Epic/Level: {this.props.title}</h1>
+                {this.recursiveRenderSkills(this.props.tree)}
+            </div>
+        );
+    };
+
     render() {
         const modifiedTree = this.modifyIconPaths(this.props.tree);
+        const { showFullInfo } = this.state;
 
         return (
-            <div onClick={this.handleSkillClick}>
-                <SkillProvider>
-                    <SkillTreeGroup theme={{
-                        nodeBorderColor: 'grey',
-                        nodeBorderRadius: '32px',
-                        nodeHoverBorder: 'none',
-                        edgeBorder: '1px solid grey'
-                    }}>
-                        {({ skillCount }: SkillGroupDataType) => (
-                            <SkillTree
-                                treeId={this.props.treeId}
-                                title={this.props.title}
-                                data={modifiedTree}
-                                collapsible
-                                handleSave={this.props.handleSave}
-                            />
-                        )}
-                    </SkillTreeGroup>
-                </SkillProvider>
+            <div>
+                <div onClick={this.handleSkillClick}>
+                    <SkillProvider>
+                        <SkillTreeGroup theme={{
+                            nodeBorderColor: 'grey',
+                            nodeBorderRadius: '32px',
+                            nodeHoverBorder: 'none',
+                            edgeBorder: '1px solid grey'
+                        }}>
+                            {() => (
+                                <SkillTree
+                                    treeId={this.props.treeId}
+                                    title={this.props.title}
+                                    data={modifiedTree}
+                                    collapsible
+                                    handleSave={this.props.handleSave}
+                                />
+                            )}
+                        </SkillTreeGroup>
+                    </SkillProvider>
+                </div>
+                <div style={{ marginTop: '20px' }}>
+                    <Button variant="contained" color="primary" onClick={this.toggleFullInfo}>
+                        {showFullInfo ? "Hide" : "Show"} the complete training resources
+                    </Button>
+
+                </div>
+                {showFullInfo && this.renderFullInfo()}
+
+
             </div>
         );
     }
